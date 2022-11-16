d340419b6826e689ca1a81e2886dcb026119e9b7
Tully <69699159+tullyc9@users.noreply.github.com>
2022-11-15 20:43:31 -0600

    Update out.lua

--- out.lua
+++ out.lua
@@ -1,8 +1,7 @@
 -- Made By Tully#2697, Henry1887#6969, And Many Others.
--- This Was NOT a Collab, I (Tully#2697) Edited Code From Both Of These Developers.
--- I Will Not Release The Obfuscation Software That I Used (That Way People Cant Steal My Code).
+-- This Was NOT a Collab, I (Tully#2697) Edited Code From These Developers.
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Solx69/Shit-Boy-Hub-Main/main/Master.lua'))();
 loadstring(game:HttpGet('https://pastebin.com/raw/8pZ3Yvrq'))();
-wait(1)
+wait(8)
 loadstring(game:HttpGet('https://pastebin.com/raw/t1ewTMnx'))();
